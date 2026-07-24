3b4f02d3bb01baea1ed9b71e26802540c27a8f2e
Aizenbens <sheninozen@gmail.com>
2026-07-24 17:27:50 +0100

    Create engine.js

---
+++ public/js/engine.js
@@ -0,0 +1,19 @@
+// Game Engine
+
+export class Engine {
+  constructor() {
+    console.log("Engine started");
+  }
+
+  start() {
+    console.log("Game running...");
+  }
+
+  update(delta) {
+    // تحديث اللعبة
+  }
+
+  render() {
+    // الرسم
+  }
+}
